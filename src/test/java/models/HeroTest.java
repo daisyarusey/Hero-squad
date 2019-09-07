503f9b112c011bbf4c33e33819b05b6890d49a7d
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-08 00:05:40 +0300

    remove tests for getAll, clearAllTasks, findById, update and deleteTask methods

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -17,7 +17,7 @@ public class HeroTest {
 
     @After
     public void tearDown() throws Exception {
-        Hero.clearAllHeroes();;
+
     }
     @Test
     public void NewHeroObjectGetsCorrectlyCreated_true() throws Exception {
@@ -55,19 +55,8 @@ public class HeroTest {
         assertEquals(1, hero.getId());
     }
 
-    @Test
-    public void AllHeroAreCorrectlyReturned_true() {
-        Hero hero = setupNewHero();
-        Hero otherHero = new Hero ("Iron Fist",24,"glowing hand","Weak");
-        assertEquals(2, Hero.getAll().size());
-    }
-    @Test
-    public void AllHeroReturnAllInstances_ofHero_true() {
-        Hero hero = setupNewHero();
-        Hero otherHero = new Hero ("Iron Fist",24,"glowing hand","Weak");
-        assertTrue(Hero.getAll().contains(hero));
-        assertTrue(Hero.getAll().contains(otherHero));
-    }
+
+
 
     @Test
     public void getCreatedAt_instantiatesWithCurrentTime_today() throws Exception{
@@ -75,35 +64,12 @@ public class HeroTest {
         assertEquals(LocalDateTime.now().getDayOfWeek(), hero.getCreatedAt().getDayOfWeek());
     }
 
-    @Test
-    public void findReturnsCorrectHero() throws Exception {
-        Hero hero = setupNewHero();
-        assertEquals(1, Hero.findById(hero.getId()).getId());
-    }
 
-    @Test
-    public void findReturnsCorrectHeroWhenMoreThanOneHeroExists() throws Exception {
-        Hero hero = setupNewHero();
-        Hero otherHero = new Hero("Iron Fist",24,"glowing hand","Weak");
-        assertEquals(2, Hero.findById(otherHero.getId()).getId());
-    }
 
-    @Test
-    public void deleteDeletesASpecificTask() throws Exception {
-        Hero hero = setupNewHero();
-        Hero otherHero = new Hero("Iron Fist",24,"glowing hand","Weak");
-        hero.deleteHero();
-        assertEquals(1, Hero.getAll().size());
-        assertEquals(Hero.getAll().get(0).getId(), 2);
-    }
 
-    @Test
-    public void deleteAllTasksDeletesAllTasks() throws Exception {
-        Hero hero = setupNewHero();
-        Hero otherHero = setupNewHero();
-        Hero.clearAllHeroes();
-        assertEquals(0, Hero.getAll().size());
-    }
+
+
+
 
     public Hero setupNewHero(){
         return new Hero("Luke Cage",30,"Bullet proof","emotional");

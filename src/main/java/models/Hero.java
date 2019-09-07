a445bf42675dfd70da425409fe8335b537e5ce35
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-07 13:24:18 +0300

    Declare variable age and create method getAge to return the age

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,12 +1,19 @@
 package models;
 
 public class Hero {
+    private  int age;
     private String name;
+
     public Hero(String name, int age, String power, String weakness) {
         this.name=name;
+        this.age=age;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }

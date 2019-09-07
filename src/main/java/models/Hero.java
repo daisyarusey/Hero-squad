820ad289ae6eee58cc084137cc99fa91e22df8fa
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-07 13:12:46 +0300

    Declare variable name and create method getName to return the name

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,6 +1,12 @@
 package models;
 
 public class Hero {
+    private String name;
     public Hero(String name, int age, String power, String weakness) {
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
     }
 }

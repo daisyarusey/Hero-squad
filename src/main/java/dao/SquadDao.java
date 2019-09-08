1f6e9d9e5ed5f73d5e37be526de3959bf9b81c99
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-08 12:55:12 +0300

    Add methods to list squad,create,read,update and delete squads

--- src/main/java/dao/SquadDao.java
+++ src/main/java/dao/SquadDao.java
@@ -1,4 +1,23 @@
 package dao;
+import models.Squad;
+import models.Hero;
+
+import java.util.List;
 
 public interface SquadDao {
+    //LIST
+    List<Squad> getAll();
+
+    //CREATE
+    void add (Squad squad);
+
+    //READ
+    Squad findById(int id);
+
+    //UPDATE
+    void update(int id, String name,int maxSize, String cause);
+
+    //DELETE
+    void deleteById(int id);
+    void clearAllSquads();
 }

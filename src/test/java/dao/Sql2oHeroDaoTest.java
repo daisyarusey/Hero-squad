126854ed983f20ec9d81ca5e82f22a513c0b3b19
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-08 12:33:28 +0300

    test for finding existing hero with id

--- src/test/java/dao/Sql2oHeroDaoTest.java
+++ src/test/java/dao/Sql2oHeroDaoTest.java
@@ -4,15 +4,15 @@ import models.Hero;
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
-public class Sql2oTaskDaoTest {
-    private Sql2oTaskDao taskDao; //ignore me for now. We'll create this soon.
+public class Sql2oHeroDaoTest {
+    private Sql2oHeroDao heroDao; //ignore me for now. We'll create this soon.
     private Connection conn; //must be sql2o class conn
 
     @Before
     public void setUp() throws Exception {
         String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "", "");
-        HeroDao = new Sql2oTaskDao(sql2o); //ignore me for now
+        heroDao = new Sql2oHeroDao(sql2o); //ignore me for now
         conn = sql2o.open(); //keep connection open through entire test so it does not get erased
     }
 
@@ -20,4 +20,22 @@ public class Sql2oTaskDaoTest {
     public void tearDown() throws Exception {
         conn.close();
     }
+
+    @Test
+    public void addingCourseSetsId() throws Exception {
+        Hero hero = new Hero ("Iron Fist",37,"glowing hand","weak");
+        int originalHeroId = hero.getId();
+        heroDao.add(hero);
+        assertNotEquals(originalHeroId, hero.getId()); //how does this work?
+    }
+
+    @Test
+    public void existingHeroesCanBeFoundById() throws Exception {
+        Hero hero = new Hero ("Iron Fist",37,"glowing hand","weak");
+        heroDao.add(hero); //add to dao (takes care of saving)
+        Hero foundHero = heroDao.findById(hero.getId()); //retrieve
+        assertEquals(hero, foundHero); //should be the same
+    }
+
+
 }

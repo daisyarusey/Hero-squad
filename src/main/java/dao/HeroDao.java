7c96931b9f0fd7c7bee425ed48cf149b9e924391
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-08 12:38:12 +0300

    Create interface with methods to update, delete,create,read a hero

--- src/main/java/dao/HeroDao.java
+++ src/main/java/dao/HeroDao.java
@@ -1,4 +1,27 @@
 package dao;
 
-public class HeroDao {
+import models.Hero;
+
+import java.util.List;
+
+public interface HeroDao {
+    // LIST
+    List<Hero> getAll();
+
+    // CREATE
+    void add(Hero hero) ;
+
+    // READ
+    Hero findById(int id);
+
+
+    // UPDATE
+    void update(int id, String name, int age, String power, String weakness);
+
+    // DELETE
+
+
+    void deleteById(int id);
+
+    void clearAllHeroes();
 }

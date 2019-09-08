6b0e64e40755a680d63452a2a9caf7e30f7e280a
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-08 12:39:17 +0300

    Add methods

--- src/main/java/dao/Sql2oHeroDao.java
+++ src/main/java/dao/Sql2oHeroDao.java
@@ -4,7 +4,7 @@ import models.Hero;
 import org.sql2o.*;
 import java.util.List;
 
-public class Sql2oHeroDao  {
+public class Sql2oHeroDao implements HeroDao {
 
     private final Sql2o sql2o;
 
@@ -12,34 +12,74 @@ public class Sql2oHeroDao  {
         this.sql2o = sql2o;
     }
 
+
     @Override
     public void add(Hero hero) {
-        String sql = "INSERT INTO heroes (name) VALUES (:name)"; //raw sql
-        try(Connection con = sql2o.open()){ //try to open a connection
-            int id = (int) con.createQuery(sql, true) //make a new variable
-                    .bind(hero) //map my argument onto the query so we can use information from it
-                    .executeUpdate() //run it all
-                    .getKey(); //int id is now the row number (row “key”) of db
-            hero.setId(id); //update object to set id now from database
-        } catch (Sql2oException ex) {
-            System.out.println(ex); //oops we have an error!
+        String sql = "INSERT INTO heroes (name,age,power,weakness) VALUES (':name',':age',':power',':weakness')";
+        try(Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(hero)
+                    .executeUpdate()
+                    .getKey();
+            hero.setId(id);
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+    @Override
+    public List<Hero> getAll() {
+        try (Connection con =sql2o.open()){
+            return con.createQuery("SELECT * FROM heroes")
+                    .executeAndFetch(Hero.class);
         }
     }
 
-    @Override
-    public List<Hero> getAll() {
-        try(Connection con = sql2o.open()){
-            return con.createQuery("SELECT * FROM heroes") //raw sql
-                    .executeAndFetch(Hero.class); //fetch a list
-        }
-    }
 
     @Override
     public Hero findById(int id) {
         try(Connection con = sql2o.open()){
-            return con.createQuery("SELECT * FROM tasks WHERE id = :id")
+            return con.createQuery("SELECT * FROM heroes WHERE id = :id")
                     .addParameter("id", id) //key/value pair, key must match above
                     .executeAndFetchFirst(Hero.class); //fetch an individual item
+        }
+    }
+
+    @Override
+    public void update(int id, String newName, int newAge, String newPower, String newWeakness){
+        String sql = "UPDATE heroes SET name = :name, age= :age, power=:power,weakness=:weakness WHERE id=:id";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("name", newName)
+                    .addParameter("age",newAge)
+                    .addParameter("power",newPower)
+                    .addParameter("weakness",newWeakness)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from heroes WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void clearAllHeroes() {
+        String sql = "DELETE from heroes";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
         }
     }
 

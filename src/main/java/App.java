0fb46001a2d074e44cc55af36ac9744475b3c726
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-08 12:40:16 +0300

    Refactor the routers to incooperate the dao model

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,94 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import dao.Sql2oHeroDao;
+import models.Hero;
+import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String connectionString = "jdbc:h2:~/hero-squad.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        Sql2oHeroDao heroDao = new Sql2oHeroDao(sql2o);
+
+        //get: delete all heroes
+        get("/heroes/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            heroDao.clearAllHeroes();
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        //get: delete an individual hero
+        get("/heroes/:id/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfHeroToDelete = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            heroDao.deleteById(idOfHeroToDelete);
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        //get: show all tasks
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> heroes = (ArrayList<Hero>) heroDao.getAll();
+            model.put("heroes", heroes);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/heroes/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "heroes-form.hbs");
+        }, new HandlebarsTemplateEngine());
+//
+//        // process new hero form
+        post("/heroes", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = req.queryParams("name");
+            String power = req.queryParams("power");
+            String weakness = req.queryParams("weakness");;
+            int age = Integer.parseInt(req.queryParams("age"));
+            Hero newHero = new Hero(name,age,power,weakness);
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/heroes/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfHeroToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            Hero foundHero = heroDao.findById(idOfHeroToFind);
+            model.put("hero", foundHero);
+            return new ModelAndView(model, "heroes-details.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/heroes/:id/update", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfHeroToEdit = Integer.parseInt(req.params("id"));
+            Hero editHero = heroDao.findById(idOfHeroToEdit);
+            model.put("editHero", editHero);
+            return new ModelAndView(model, "Heroes-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //process a form to update a hero
+        post("/heroes/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String newName = req.queryParams("name");
+            int idOfHeroToEdit = Integer.parseInt(req.params("id"));
+            int newAge= Integer.parseInt(req.params("age"));
+
+            String newPower=req.queryParams("power");
+            String newWeakness=req.queryParams("weakness");;
+            heroDao.update(idOfHeroToEdit, newName,newAge,newPower,newWeakness);
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+    }
 }

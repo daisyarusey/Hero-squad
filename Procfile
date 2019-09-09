f3cf8cb5a2c959ed3c6725eb035c44b5871e7a0f
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-09 18:13:31 +0300

    Create Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/Hero-squad/bin/Hero-squad
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,6 +14,21 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
+
         staticFileLocation("/public");
         String connectionString = "jdbc:h2:~/hero-squad.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "", "");
